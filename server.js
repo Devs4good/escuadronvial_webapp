ff5a777d4c7f91223a352bea2a6327b0802fe10d
Rodrigo Ezequiel Papadopulo <rpapadopulo@baufest.com>
2019-09-08 10:31:39 -0300

    Arreglo en server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/escuadronvial'));
+app.use(express.static(__dirname + '/dist/EscuadronVial'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/escuadronvial/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/EscuadronVial/index.html'));
 });
 
 // default Heroku PORT
